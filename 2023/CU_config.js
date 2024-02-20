d94503740ec15e7ed2b53d8ef52f35ad423eadb8
lstone24 <lostonel22@stu.lok12.org>
2024-02-19 21:35:54 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,20 +1,20 @@
 var config_data = `
 {
   "title": "Scouting PASS 2023",
-  "page_title": "Charged Up",
+  "page_title": "Crescendo",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
       "code": "s",
       "type": "scouter",
       "size": 5,
-      "maxSize": 5,
+      "maxSize": 15,
       "required": "true"
     },
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023mijac",
+      "defaultValue": "2023miber",
       "required": "true",
       "disabled": "true"
     },
@@ -55,79 +55,56 @@ var config_data = `
       "min": 1,
       "max": 99999
     },
-    { "name": "Auto Start Position",
+    { "name": "Did they show up?",
       "code": "as",
-      "type": "clickable_image",
-      "filename": "2023/field_image.png",
-      "clickRestriction": "one",
-      "shape": "circle 5 black red true"
+      "type": "bool"
     }
   ],
   "auton": [
-    { "name": "Cross the line",
-	  "code": "acl",
+    { "name": "Leave Starting Zone",
+	  "code": "al",
 	  "type": "bool"
 	},
-    { "name": "Scored in Speaker",
-      "code": "asps",
-      "type": "counter"
-    },
-    { "name": "Attempted in Speaker",
-      "code": "aspa",
-      "type": "counter"
-    },
     { "name": "Scored in Amp",
-      "code": "aamps",
-      "type":"counter"
+      "code": "asa",
+      "type": "counter"
     },
-	( "name": "Attempted on Amp",
-	  "code": "aampa",
+    { "name": "Attempted in Amp",
+      "code": "aaa",
+      "type": "counter"
+    },
+    { "name": "Scored in Speaker",
+      "code": "ass",
+      "type": "counter"
+    },
+	( "name": "Attempted on Speaker",
+	  "code": "aas",
 	  "type": "counter"
 	}
   ],
   "teleop": [
-    { "name": "Cycle Timer",
-      "code": "tct",
-      "type": "cycle"
-    },
-    { "name": "Grid Scoring",
-      "code": "tsg",
-      "type": "clickable_image",
-      "filename": "2023/grid_image.png",
-      "dimensions": "9 4",
-      "clickRestriction": "onePerBox",
-      "toggleClick": "true",
-      "showFlip": "false",
-      "showUndo": "false",
-      "shape": "circle 12 black red true",
-      "cycleTimer": "tct"
-    },
-    { "name": "Feeder Count<br>(Fed another bot)",
-      "code": "tfc",
+    { "name": "Scored in Amp",
+      "code": "tsa",
       "type": "counter"
     },
-    { "name": "Was Fed<br>Game Pieces",
-      "code": "wf",
-      "type": "bool"
+    { "name": "Attempted in Amp",
+      "code": "taa",
+	  "type": "counter"
+	},
+	{ "name": "Scored in Speaker",
+      "code": "tss",
+      "type": "counter"
     },
-    { "name": "Was Defended",
-      "code": "wd",
-      "type": "bool"
-    },
-    { "name": "Who Defended this bot",
-      "code": "who",
-      "type": "text"
-    },
-    { "name": "Smart Placement<br>(creates Links)",
-      "code": "lnk",
-      "type": "bool"
-    },
-    { "name": "Floor Pickup",
-      "code": "fpu",
+	{ "name": "Who defended them?<br>(if not defended, ignore)",
+	  "code": "wdt",
+	  "type": "text"
+	},
+    { "name": "Pickup From:",
+      "code": "tpu",
       "type": "radio",
       "choices": {
-        "o": "Cones<br>",
-        "u": "Cubes<br>",
+        "s": "Source<br>",
+        "f": "Floor<br>",
         "b": "Both<br>",
         "x": "Not Attempted"
       },
@@ -135,7 +112,7 @@ var config_data = `
     }
   ],
   "endgame": [
-    { "name": "Docking Timer",
+    { "name": "Stage Timer",
       "code": "dt",
       "type": "timer"
     },
@@ -144,34 +121,24 @@ var config_data = `
       "type":"radio",
       "choices": {
         "p": "Parked<br>",
-        "d": "Docked (Not Engaged)<br>",
-        "e": "Engaged<br>",
+		"o": "Onstage<br>",
+		"s": "Onstage (Spotlit)<br>",
+		"h": "Harmony<br>",
         "a": "Attempted but failed<br>",
         "x": "Not attempted"
       },
       "defaultValue": "x"
     },
-    { "name": "Total # of alliance<br>robots docked/engaged",
-      "code": "dn",
+	{ "name": "Scored in Trap",
+      "code": "sit",
+      "type": "counter"
+    },
+	{ "name": "Attempted in Speaker",
+      "code": "ait",
       "type": "counter"
     }
   ],
   "postmatch": [
-    { "name": "Driver Skill",
-      "code": "ds",
-      "type": "radio",
-      "choices": {
-        "n": "Not Effective<br>",
-        "a": "Average<br>",
-        "v": "Very Effective<br>",
-        "x": "Not Observed"
-      },
-      "defaultValue": "x"
-    },
-    { "name": "Links Scored",
-      "code": "ls",
-      "type": "counter"
-    },
     { "name": "Defense Rating",
       "code": "dr",
       "type": "radio",
@@ -184,37 +151,16 @@ var config_data = `
       },
       "defaultValue": "x"
     },
-    { "name": "Swerve drive?",
-      "code": "sd",
-      "type": "bool"
-    },
-    { "name": "Speed Rating",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "1 (slow)<br>",
-        "2": "2<br>",
-        "3": "3<br>",
-        "4": "4<br>",
-        "5": "5 (fast)"
-      },
-      "defaultValue":"3"
-    },
+	{ "name": "Who did they defend?",
+	  "code": "who",
+	  "type": "text"
+	},
     { "name": "Died/Immobilized",
       "code": "die",
       "type": "bool"
     },
     { "name": "Tippy<br>(almost tipped over)",
       "code": "tip",
-      "type": "bool"
-    },
-    { "name": "Dropped Cones (>2)",
-      "code": "dc",
-      "type": "bool"
-    },
-    { "name": "Make good<br>alliance partner?",
-      "tooltip": "Would you want this robot on your alliance in eliminations?",
-      "code": "all",
       "type": "bool"
     },
     { "name": "Comments",
