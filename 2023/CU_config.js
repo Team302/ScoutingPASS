0257fa8636c4b461c05196ee968b6967096799ff
lstone24 <lostonel22@stu.lok12.org>
2024-02-23 18:15:14 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -126,7 +126,6 @@ var config_data = `
       "choices": {
         "p": "Parked<br>",
 		"o": "Onstage<br>",
-		"s": "Onstage (Spotlit)<br>",
 		"h": "Harmony<br>",
         "a": "Attempted but failed<br>",
         "x": "Not attempted"
