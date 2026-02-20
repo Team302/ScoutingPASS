5d01b916680cf5544f92656f6ab6aa57552cd0dc
Jacobmyers07 <ogatomicbricks@gmail.com>
2026-02-20 18:37:10 -0500

    Update RS_Pit_config.js

--- 2025/RS_Pit_config.js
+++ 2025/RS_Pit_config.js
@@ -1,87 +1,73 @@
-
 var config_data = `
 {
-  "title": "Reefscape 2025",
-  "page_title": "Reefscape",
+  "title": "Team 302 2026 Pit Scouting",
+  "page_title": "Rebuilt",
   "pitConfig": "true",
   "checkboxAs": "10",
   "prematch": [
-    { "name": "Scouter Name1",
-	  "code": "s", 
-	  "type": "text",
-	  "size": 10,
-	  "maxsize": 15
-	},
+    { "name": "Scouter Name",
+      "code": "s",
+      "type": "text",
+      "size": 10,
+      "maxsize": 15
+    },
     { "name": "Team Number",
       "code": "t",
-      "type": "number"
-    },
-    { "name": "Dimensions",
-      "code": "dm",
       "type": "text",
       "size": 10,
       "maxSize": 15
     },
-    { "name": "Weight",
-      "code": "wei",
-      "type": "number",
-      "defaultValue": "0"
+    { "name": "Can they fit under the trench?",
+      "code": "trn",
+      "type": "radio",
+      "choices": {
+        "y": "Yes<br>",
+        "n": "No"
+      },
+      "defaultValue": "n"
+    },
+    { "name": "Can they go over the bump?",
+      "code": "bmp",
+      "type": "radio",
+      "choices": {
+        "y": "Yes<br>",
+        "n": "No"
+      },
+      "defaultValue": "n"
     },
     { "name": "Drivetrain",
       "code": "drv",
       "type": "radio",
       "choices": {
         "s": "Swerve<br>",
-        "w": "West Coast/Tank<br>",
+        "w": "Tank<br>",
         "m": "Mechanum<br>",
         "o": "Other"
       },
       "defaultValue": "o"
     },
-    { "name": "Other Drivetrain",
-      "code": "odt",
+    { "name": "Estimated Hopper Capacity (# of balls)",
+      "code": "hop",
       "type": "text",
-      "size": 20,
-      "maxSize": 50
+      "size": 5,
+      "maxSize": 10
     },
-    { "name": "Swerve Ratio (speed)",
-      "code": "sr",
-      "type": "radio",
-      "choices": {
-        "1": "L1 (8.14:1 or 12-13 ft/sec)<br>",
-        "2": "L2 (6.75:1 or 14-16 ft/sec)<br>",
-        "3": "L3 (6.12:1 or 16-18 ft/sec)<br>",
-		"d": "Doesn't Know<br>",
-        "x": "Not Swerve"
-      },
-      "defaultValue":"x"
+    { "name": "Able to Climb L1",
+      "code": "cl1",
+      "type": "bool"
     },
-	{ "name": "Coral Pickup Location",
-      "code": "asp",
-      "type": "radio",
-	  "choices": {
-		  "s": "Floor<br>",
-		  "a": "HP<br>",
-		  "b": "Both<br>",
-		  "x": "no coral<br>"
-	  },
-	  "defaultValue": "x"
+    { "name": "Able to Climb L2",
+      "code": "cl2",
+      "type": "bool"
     },
-    { "name": "Coral Place Location",
-      "code": "asc",
-      "type": "radio",
-	  "choices": {
-		  "s": "L1 only <br>",
-		  "a": "All Levels<br>",
-		  "b": "None<br>",
-		  "x": "L2-L4<br>"
-	  },
-	  "defaultValue": "x"
+    { "name": "Able to Climb L3",
+      "code": "cl3",
+      "type": "bool"
     },
-	    { "name": "Auton Scoring",
-      "code": "asg",
+    { "name": "Where they shoot from",
+      "code": "sht",
       "type": "clickable_image",
-      "filename": "2023/grid_image.png",
+      "filename": "2020/field_image.png",
       "dimensions": "9 4",
       "clickRestriction": "onePerBox",
       "toggleClick": "true",
@@ -89,36 +75,7 @@ var config_data = `
       "showUndo": "false",
       "shape": "circle 12 black red true"
     },
-        { "name": "Main strategy",
-      "code": "str",
-      "type": "radio",
-	  "choices": {
-		  "s": "Algae <br>",
-		  "a": "Coral<br>",
-		  "b": "Defense<br>",
-		  "x": "Anything<br>"
-	  },
-	  "defaultValue": "x"
-    },
-	{ "name": "How many coral in auto?",
-	  "code": "ac", 
-	  "type": "counter",
-	  "defaultValue": 0
-	},
-	{ "name": "How many coral in teleop?",
-	  "code": "tc", 
-	  "type": "counter",
-	  "defaultValue": 0
-	},
-	{ "name": "Able to deep climb?",
-	  "code": "tra",
-	  "type": "bool"
-	},
-	{ "name": "Able to shallow climb?",
-	  "code": "clm",
-	  "type": "bool"
-	},
-	{ "name": "Comments",
+    { "name": "Comments",
       "code": "co",
       "type": "text",
       "size": 20,
